40ba1fe3aa16753d8fdd7d252c538a39ba9eb8ac
Stephen <stephengroat@users.noreply.github.com>
2017-07-19 16:42:05 -0700

    Create aws.rb

---
+++ collect/aws.rb
@@ -0,0 +1,15 @@
+require "open-uri"
+require "json"
+
+namespace :collect do
+  task :aws, %i[region service] do |_t, args|
+    args.with_defaults(region: ".*", service: ".*")
+
+    list = open("https://ip-ranges.amazonaws.com/ip-ranges.json")
+    data_hash = JSON.parse(list.read)
+    data_hash["prefixes"].each do |prefix|
+      puts prefix["ip_prefix"] if prefix["region"] =~ /#{args[:region]}/ && \
+                                  prefix["service"] =~ /#{args[:service]}/
+    end
+  end
+end

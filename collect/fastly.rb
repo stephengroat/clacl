3f5c11698bf9950971263838bff098321f5d1eaa
Stephen <stephen@egroat.com>
2017-12-19 10:47:59 -0800

    Add fastly

---
+++ collect/fastly.rb
@@ -0,0 +1,13 @@
+require "open-uri"
+require "json"
+
+namespace :collect do
+  task :fastly do
+
+    list = open("https://api.fastly.com/public-ip-list")
+    data_hash = JSON.parse(list.read)
+    data_hash["addresses"].each do |address|
+      puts address
+    end
+  end
+end

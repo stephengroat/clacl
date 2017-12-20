5c76e619d326309d6d24b7193a68234cae8982d4
Stephen <stephen@egroat.com>
2017-12-19 16:08:18 -0800

    add cachefly

---
+++ collect/cachefly.rb
@@ -0,0 +1,10 @@
+require "open-uri"
+
+namespace :collect do
+  task :cachefly do
+    list = open("https://admin.cachefly.com/ips/rproxy.txt")
+    list.each do |ip|
+      puts ip
+    end
+  end
+end

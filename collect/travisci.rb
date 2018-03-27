b99be586bfdca70df82d8bc58e869cb1ebad8a01
Stephen <stephengroat@users.noreply.github.com>
2018-03-27 10:51:14 -0700

    add url source

--- collect/travisci.rb
+++ collect/travisci.rb
@@ -2,6 +2,7 @@ require "resolv"
 
 namespace :collect do
   task :travisci  do |_t|
+    # comes from https://docs.travis-ci.com/user/ip-addresses/
     addresses = Resolv.getaddresses("nat.travisci.net")
     addresses.each do |address|
       puts address + "/32"

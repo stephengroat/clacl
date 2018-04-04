0b9454bf5e5e2c5cf26251c43bc358998ba6c6ef
Stephen <stephen@egroat.com>
2018-04-04 14:06:54 -0700

    Workaround to use github tokens

--- collect/github.rb
+++ collect/github.rb
@@ -3,8 +3,8 @@ require "json"
 
 namespace :collect do
   task :github  do |_t|
-    puts JSON.parse(open("https://api.github.com/rate_limit").read)
-    list = open("https://api.github.com/meta")
+    list = open("https://api.github.com/meta", http_basic_authentication: [ENV["githubcom"].split(":")[0], ENV["githubcom"].split(":")[1]]) unless ENV["githubcom"].nil?
+    list = open("https://api.github.com/meta") if ENV["githubcom"].nil?
     data_hash = JSON.parse(list.read)
     %w[hooks git pages importer].each do |type|
       data_hash[type].each do |address|

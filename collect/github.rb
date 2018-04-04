7e1f5a7899270b410bedf52a27f0fe816691536d
Stephen <stephen@egroat.com>
2018-04-04 10:45:20 -0700

    Debug github rate limiting

--- collect/github.rb
+++ collect/github.rb
@@ -3,6 +3,7 @@ require "json"
 
 namespace :collect do
   task :github  do |_t|
+    puts JSON.parse(open("https://api.github.com/rate_limit").read)
     list = open("https://api.github.com/meta")
     data_hash = JSON.parse(list.read)
     %w[hooks git pages importer].each do |type|

96a2208103309473a8ae3fec0e3f8fbca5c8c77b
Stephen <stephengroat@users.noreply.github.com>
2018-05-11 09:00:38 -0700

    cleanup and add comments

--- collect/msft.rb
+++ collect/msft.rb
@@ -5,9 +5,9 @@ namespace :collect do
   task :msft do |_t, _args|
     list = nil
 
-    options = Selenium::WebDriver::Chrome::Options.new(args: ['no-sandbox'])
-
     Dir.mktmpdir do |dir|
+      options = Selenium::WebDriver::Chrome::Options.new(args: ['no-sandbox'])
+      
       prefs = {
         prompt_for_download: false,
         default_directory: dir.to_s
@@ -21,7 +21,9 @@ namespace :collect do
       driver.quit
     end
 
+    # Get array of arrays from CSV
     data_hash = CSV.parse(list)
+    # Use first title row as keys for Hashmap
     keys = data_hash.shift
     data_hash.map! { |a| Hash[keys.zip(a)] }
     data_hash.each do |prefix|

2c3b05b693fd088db7a9b38b27663ce7dbb6a95c
Stephen <stephengroat@users.noreply.github.com>
2018-08-28 15:25:22 -0700

    work on travis w/ auth, work local, return on PRs

--- collect/github.rb
+++ collect/github.rb
@@ -3,12 +3,15 @@ require 'json'
 
 namespace :collect do
   task :github do |_t|
-    unless ENV['githubcom'].nil?
+    if !ENV['githubcom'].nil?
       list = open('https://api.github.com/meta',
                   http_basic_authentication: [ENV['githubcom'].split(':')[0],
                                               ENV['githubcom'].split(':')[1]])
+    eslif ENV['TRAVIS'].nil?
+      list = open('https://api.github.com/meta')
+    else
+      return
     end
-    list = open('https://api.github.com/meta') if ENV['githubcom'].nil?
     data_hash = JSON.parse(list.read)
     %w[hooks git pages importer].each do |type|
       data_hash[type].each do |address|

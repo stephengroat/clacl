6c0a9ccb0a88629682e0a8777de3ec95e8e1e638
Stephen <stephengroat@users.noreply.github.com>
2020-03-01 17:59:54 -0800

    Update azure.rb

--- collect/azure.rb
+++ collect/azure.rb
@@ -3,53 +3,38 @@
 require 'webdrivers'
 require 'json'
 
+MS_DOWNLOAD_URL = 'https://www.microsoft.com/en-us/download/confirmation.aspx?id='
+
 namespace :collect do
   task :azure, %i[region] do |_t, args|
     [57_062, 57_064, 57_063, 56_519].each do |id|
-      list = nil
       args.with_defaults(region: '.*')
 
       Dir.mktmpdir do |dir|
-        options = Selenium::WebDriver::Chrome::Options.new(args: ['no-sandbox'])
+        options = Selenium::WebDriver::Chrome::Options.new
 
-        prefs = {
-          prompt_for_download: false,
-          default_directory: dir.to_s,
-          directory_upgrade: true
-        }
-        options.add_preference(:download, prefs)
-        prefs = {
-          default_directory: dir.to_s
-        }
-        options.add_preference(:savefile, prefs)
-        prefs = {
-          enabled: false
-        }
-        options.add_preference(:safebrowsing, prefs)
+        options.add_preference(:download,
+                               prompt_for_download: false,
+                               directory_upgrade: true,
+                               default_directory: dir.to_s)
 
         driver = Selenium::WebDriver.for(:chrome, options: options)
 
-        driver.get("https://www.microsoft.com/en-us/download/confirmation.aspx?id=#{id}")
+        driver.get(MS_DOWNLOAD_URL + id.to_s)
 
         loop do
-          if Dir.glob("#{dir}/*.json.part").any?
-            sleep(10)
-          elsif Dir.glob("#{dir}/*.json").any?
-            break
-          else
-            sleep(10)
-          end
+          sleep(10) if Dir.glob("#{dir}/*.json.part").any?
+          break if Dir.glob("#{dir}/*.json").any?
         end
 
         list = File.open(Dir.glob("#{dir}/*.json")[0])
-        driver.quit
-      end
 
-      data_hash = JSON.parse(list.read)
-      data_hash['values'].each do |value|
-        value['properties']['addressPrefixes'].each do |address_prefix|
-          puts address_prefix
+        JSON.parse(list.read)['values'].each do |value|
+          value['properties']['addressPrefixes'].each do |address_prefix|
+            puts address_prefix
+          end
         end
+        driver.quit
       end
     end
   end
